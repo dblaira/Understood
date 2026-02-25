c4c3515835dd6c603abe2627f604304ec293e260
dblaira <adamdblair@gmail.com>
2026-02-24 21:04:31 -0800

    Initial Commit

---
+++ Understood/UnderstoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstoodApp.swift
+//  Understood
+//
+//  Created by Adam Blair on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct UnderstoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
